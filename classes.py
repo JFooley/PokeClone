1d79e9e6c45f69b75485d33be5f26a1637aaa4f4
João Gabriel <gabriel.lima.almeida@gmail.com>
2024-01-29 19:11:13 -0300

    update

--- classes.py
+++ classes.py
@@ -26,3 +26,41 @@ class Pokemon:
         self.legendary = legendary
         self.moves = moves
 
+class Dao:
+	def __init__(self, id, name, level: int, type1, type2, STR: int, POW: int, RES: int, MRES: int, AGI: int, HP: int, currentHP: int, effect: list):
+		self.id = id
+		self.name = name
+		self.level = level
+		self.type1 = type1
+		self.type2 = type2
+		
+		self.HP = HP
+		self.STR = STR
+		self.RES = RES
+		self.POW = POW
+		self.MRES = MRES
+		self.AGI = AGI
+		
+		self.curretHP = currentHP
+		self.effect = effect
+		
+class Effect: 
+	def __init__(self, type, HP, STR, RES, POW, MRES, AGI, INIT: int, foeHP, foeSTR, foeRES, foePOW, foeMRES, foeAGI, foeINIT: int):
+		self.type = type
+		
+		self.HP = HP
+		self.STR = STR
+		self.RES = RES
+		self.POW = POW
+		self.MRES = MRES
+		self.AGI = AGI
+		self.INIT = INIT
+		
+		self.foeHP = foeHP
+		self.foeSTR = foeSTR
+		self.foeRES = foeRES
+		self.foePOW = foePOW
+		self.foeMRES = foeMRES
+		self.foeAGI = foeAGI
+		self.foeINIT = foeINIT
+		
